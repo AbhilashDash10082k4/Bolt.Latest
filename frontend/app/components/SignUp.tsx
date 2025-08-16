95e493c1f25c50affa4ad8e474d373cacff3cda8
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2025-08-17 01:14:42 +0530

    made some other changes

--- frontend/app/components/SignUp.tsx
+++ frontend/app/components/SignUp.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import {
-  Button,
-  Label,
-  Modal,
-  ModalBody,
-} from "flowbite-react";
+import { Button, Label, Modal, ModalBody } from "flowbite-react";
 import Link from "next/link";
 import { useRef, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { VscGithub } from "react-icons/vsc";
+import SignIn, { SignInWithGithub } from "../lib/actions";
 
 interface Prop {
   title: string;
@@ -20,6 +16,7 @@ export function SignUp({ title }: Prop) {
 
   return (
     <>
+    
       <div className="relative group w-full sm:w-auto">
         <div
           className="absolute inset-0 -m-2 rounded-full
@@ -29,7 +26,6 @@ export function SignUp({ title }: Prop) {
                      transition-all duration-300 ease-out
                      group-hover:opacity-60 group-hover:blur-group-hover:-m-3"
         ></div>
-
         <button
           className="relative z-10 px-4 py-2 sm:px-3 text-xs sm:text-sm font-semibold text-black 
                      bg-gradient-to-br from-gray-100 to-gray-300 rounded-full 
@@ -41,7 +37,12 @@ export function SignUp({ title }: Prop) {
           {title}
         </button>
       </div>
-
+      <div className="pl-2">
+        <button className="px-4 py-2 rounded-xl bg-gradient-to-l from-zinc-900 via-zinc-600 to-zinc-900 hover:drop-shadow-white text-zinc-300 hover:bg-zinc-500 hover:text-white transition">
+        Login
+      </button>
+      </div>
+      
       <Modal
         show={openModal}
         size="md"
@@ -97,11 +98,17 @@ export function SignUp({ title }: Prop) {
               <Label htmlFor="password" className="mb-2 block text-gray-300">
                 Your password
               </Label>
-              <input id="password" placeholder="••••••••" type="password" required className="w-full px-4 py-2 rounded-lg 
+              <input
+                id="password"
+                placeholder="••••••••"
+                type="password"
+                required
+                className="w-full px-4 py-2 rounded-lg 
                bg-zinc-800 text-gray-200 placeholder-gray-500
                border border-zinc-700 
                focus:outline-none focus:ring-2 focus:ring-zinc-400 focus:border-transparent
-               transition duration-200"/>
+               transition duration-200"
+              />
             </div>
             <div className="flex justify-end">
               <Link href="#" className="text-sm text-zinc-400 hover:underline">
@@ -122,7 +129,8 @@ export function SignUp({ title }: Prop) {
               <hr className="flex-grow border-gray-700" />
             </div>
             <div className="flex flex-col space-y-3">
-              <button
+              <form action={SignIn}>
+                <button
                 className="w-full px-4 py-2 rounded-lg font-medium 
                                  bg-zinc-800 border border-zinc-700 text-white 
                                  hover:bg-zinc-700 transition flex items-center justify-center space-x-2"
@@ -130,7 +138,8 @@ export function SignUp({ title }: Prop) {
                 <FcGoogle className="text-red-400" />
                 <span>Continue with Google</span>
               </button>
-
+              </form>
+              <form action={SignInWithGithub}>
               <button
                 className="w-full px-4 py-2 rounded-lg font-medium 
                                  bg-zinc-800 border border-zinc-700 text-white 
@@ -139,12 +148,15 @@ export function SignUp({ title }: Prop) {
                 <VscGithub className="text-white" />
                 <span>Continue with GitHub</span>
               </button>
+              </form>
             </div>
             <div className="items-center flex justify-center text-sm font-medium text-gray-400">
               <span>Already registered?</span>
-              <span><Link href="#" className="text-blue-400 hover:underline">
-                Log In
-              </Link></span>
+              <span>
+                <Link href="#" className="text-blue-400 hover:underline">
+                  Log In
+                </Link>
+              </span>
             </div>
           </div>
         </ModalBody>
